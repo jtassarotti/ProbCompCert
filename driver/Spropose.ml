4be36a0a6a4c4ca585e358a19dfce7f0de0265ab
Jean-Baptiste Tristan <tristanj@bc.edu>
2022-05-06 15:25:57 -0400

    Removed useless RNG

--- driver/Spropose.ml
+++ driver/Spropose.ml
@@ -6,44 +6,17 @@ let renderPropose global_state struct_type struct_vars =
     match (t, var.Stan.vd_dims) with
     | (t, [])                   ->
       "  " ^ (String.concat "\n  " [
-          "eps = my_randn(0.0,1.0);";
+          "eps = randn(0.0,1.0);";
            "c->" ^ v ^" = s->" ^ v ^" + eps;";
       ])
     | _ -> raise (NIY_propose "renderPropose.proposeField: incomplete for this type")
   in
   String.concat "\n" ([
-"double my_randn (double mu, double sigma)";
-"{";
-"  double U1, U2, W, mult;";
-"  static double X1, X2;";
-"  static int call = 0;";
-"";
-"  if (call == 1)";
-"    {";
-"      call = !call;";
-"      return (mu + sigma * (double) X2);";
-"    }";
-"  do";
-"    {";
-"      U1 = -1 + ((double) rand () / RAND_MAX) * 2;";
-"      U2 = -1 + ((double) rand () / RAND_MAX) * 2;";
-"      W = pow (U1, 2) + pow (U2, 2);";
-"    }";
-"  while (W >= 1 || W == 0);";
-"";
-"  mult = sqrt ((-2 * log (W)) / W);";
-"  X1 = U1 * mult;";
-"  X2 = U2 * mult;";
-"";
-"  call = !call;";
-"" ;
-"  return (mu + sigma * (double) X1);";
-"}";
     ("void* propose (void * opaque_state) {");
     ("  struct " ^ struct_type ^ "* s = (struct " ^ struct_type ^ "*) opaque_state;");
     ("  struct " ^ struct_type ^ "* c = malloc(sizeof (struct " ^ struct_type ^ "));");
     ("double eps;")
-  ] @ (List.map proposeField struct_vars) @ [
+      ] @ (List.map proposeField struct_vars) @ [
     ("  return c;");
     "}";
     "";
