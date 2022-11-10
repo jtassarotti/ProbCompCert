fbb77e7a00a7cb10292b5faf41b84ded842c8e29
Joseph Tassarotti <jt4767@nyu.edu>
2022-11-10 11:54:47 -0500

    Tweaks to hierarchical and normal in regression

--- stanfrontend/tests/bench/hierarchical_linear_regression/code.stan
+++ stanfrontend/tests/bench/hierarchical_linear_regression/code.stan
@@ -8,15 +8,17 @@ parameters {
   real gamma;
   real alpha[2];
   real beta[2];
+  real <lower=0.1>sigma;
 }
 model {
   mu ~ normal(0, 3);
   gamma ~ normal(0, 3);
+  sigma ~ normal(1,.1);
   for (i in 1:2) {
     alpha[i] ~ normal(mu, 1);
     beta[i] ~ normal(gamma, 1);
   }
   for (i in 1:200) {
-    y[i] ~ normal(alpha[class[i]] + beta[class[i]] * x[i], 1);
+    y[i] ~ normal(alpha[class[i]] + beta[class[i]] * x[i], sigma);
   }
 }
--- stanfrontend/tests/bench/hierarchical_linear_regression/generate_data.py
+++ stanfrontend/tests/bench/hierarchical_linear_regression/generate_data.py
@@ -1,17 +1,18 @@
 import json
 from numpy import *
 
-sigma = 1
+hyper_sigma = 1
 
 mu = round(random.normal(0, 3), 3)
 gamma = round(random.normal(0, 3), 3)
+sigma = round(max(0.0, random.normal(1, .1)), 3)
 
 num_classes = 2
 
-alpha = [round(random.normal(mu, sigma), 1) for x in range(num_classes)]
-beta = [round(random.normal(gamma, sigma), 1) for x in range(num_classes)]
+alpha = [round(random.normal(mu, hyper_sigma), 1) for x in range(num_classes)]
+beta = [round(random.normal(gamma, hyper_sigma), 1) for x in range(num_classes)]
 
-params = {'mu': mu, 'gamma': gamma, 'alpha': alpha, 'beta': beta}
+params = {'mu': mu, 'gamma': gamma, 'alpha': alpha, 'beta': beta, 'sigma': sigma}
 
 print(json.dumps(params))
 f = open("params.json", 'w')
@@ -21,7 +22,7 @@ x = [round(random.normal(0, 1), 2) for i in range(200)]
 cls = [random.randint(1, 3) for i in range(200)]
 y = []
 for i in range(len(x)):
-    y.append(round(random.normal(alpha[cls[i]-1] + beta[cls[i]-1] * x[i], 1), 4))
+    y.append(round(random.normal(alpha[cls[i]-1] + beta[cls[i]-1] * x[i], sigma), 4))
 
 data = {'x': x, 'y': y, 'class': cls}
 
